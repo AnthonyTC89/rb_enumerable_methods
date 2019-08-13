e19f6213987bf168a680c86548d4b29214adddcb
Anthony Tapia <ptonyptc@gmail.com>
2019-08-13 15:49:49 -0500

    Enumerable my_each_with_index done

--- iterator_methods.rb
+++ iterator_methods.rb
@@ -4,6 +4,11 @@ module Enumerable
             yield (array[i])
         end
     end
+    def self.my_each_with_index array
+        array.length.times do |i|
+            yield array[i], i
+        end
+    end
 end
 
 array = [1,2,3,4,5]
@@ -14,3 +19,9 @@ end
 # 2 4 6 8 10
 Enumerable::my_each (array) { |elem| puts elem } 
 # 2 4 6 8 10
+
+array.each_with_index do |elem,index|
+    puts "elem: " + elem.to_s + ", index:" + index.to_s
+end
+
+Enumerable::my_each_with_index (array) {|elem, index| puts "elem: " + elem.to_s + ", index:" + index.to_s} 
