883ffe278ca27ad0f356f6bf0478036f8ea3e059
Anthony Tapia <ptonyptc@gmail.com>
2019-08-16 17:01:42 -0500

    my_all? method done

--- iterator_methods.rb
+++ iterator_methods.rb
@@ -1,7 +1,7 @@
 module Enumerable
     def self.my_each array
         array.length.times do |i|
-            yield (array[i])
+            yield array[i]
         end
     end
 
@@ -14,20 +14,29 @@ module Enumerable
     def self.my_select array
         aux_array = []
         array.length.times do |i|
-            
             if yield array[i]
-                p array[i]
                 aux_array << array[i]
             end
         end
         return aux_array
     end
 
+    def self.my_all? array
+        array.length.times do |i|
+            if !yield array[i]
+                return false
+            end
+            return true
+        end
+    end
 end
 
 array = [1,2,3,4,5]
 
-p Enumerable::my_select (array) { |elem| elem.odd? }
+# p Enumerable::my_all? (array) { |elem| elem < 10 }
+# p array.all? { |elem| elem < 10}
+
+# p Enumerable::my_select (array) { |elem| elem.odd? }
 
 # p array.select{ |elem| elem.odd? }
 
