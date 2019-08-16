1aa848a15e0d7b11d586155ad8374acbcdeb76cc
Anthony Tapia <ptonyptc@gmail.com>
2019-08-16 16:51:16 -0500

    my_each, my_each_with_index, my_select methods done

--- iterator_methods.rb
+++ iterator_methods.rb
@@ -4,24 +4,45 @@ module Enumerable
             yield (array[i])
         end
     end
+
     def self.my_each_with_index array
         array.length.times do |i|
             yield array[i], i
         end
     end
+    
+    def self.my_select array
+        aux_array = []
+        array.length.times do |i|
+            
+            if yield array[i]
+                p array[i]
+                aux_array << array[i]
+            end
+        end
+        return aux_array
+    end
+
 end
 
 array = [1,2,3,4,5]
 
-array.each do |elem|
-    puts elem
-end
-# 2 4 6 8 10
-Enumerable::my_each (array) { |elem| puts elem } 
-# 2 4 6 8 10
+p Enumerable::my_select (array) { |elem| elem.odd? }
 
-array.each_with_index do |elem,index|
-    puts "elem: " + elem.to_s + ", index:" + index.to_s
-end
+# p array.select{ |elem| elem.odd? }
 
-Enumerable::my_each_with_index (array) {|elem, index| puts "elem: " + elem.to_s + ", index:" + index.to_s} 
+# array.each_with_index do |elem,index|
+#     puts "elem: " + elem.to_s + ", index:" + index.to_s
+# end
+
+# Enumerable::my_each_with_index (array) {|elem, index| puts "elem: " + elem.to_s + ", index:" + index.to_s} 
+
+
+# array.each do |elem|
+#     puts elem
+# end
+# # 2 4 6 8 10
+# Enumerable::my_each (array) { |elem| puts elem } 
+# # 2 4 6 8 10
+
+
