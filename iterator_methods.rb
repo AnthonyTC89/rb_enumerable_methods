fd9bcb9eb95b0ce899f19f546e80eadd3d559920
Anthony Tapia <ptonyptc@gmail.com>
2019-08-19 17:48:52 -0500

    update methods without arguments

--- iterator_methods.rb
+++ iterator_methods.rb
@@ -1,123 +1,116 @@
 module Enumerable
-    def self.my_each array
-        array.length.times do |i|
-            yield array[i]
+    def my_each
+        self.length.times do |i|
+            yield self[i]
         end
     end
 
-    def self.my_each_with_index array
-        array.length.times do |i|
-            yield array[i], i
+    def my_each_with_index
+        self.length.times do |i|
+            yield self[i], i
         end
     end
     
-    def self.my_select array
+    def my_select
         aux_array = []
-        array.length.times do |i|
-            if yield array[i]
-                aux_array << array[i]
+        self.length.times do |i|
+            if yield self[i]
+                aux_array << self[i]
             end
         end
         return aux_array
     end
 
-    def self.my_all? array
-        array.length.times do |i|
-            if !yield array[i]
+    def my_all? 
+        self.length.times do |i|
+            if !yield self[i]
                 return false
             end
         end
         return true
     end
 
-    def self.my_any? array
-        array.length.times do |i|
-            if yield array[i]
+    def my_any?
+        self.length.times do |i|
+            if yield self[i]
                 return true
             end
         end
         return false
     end
 
-    def self.my_none? array
-        array.length.times do |i|
-            if yield array[i]
+    def my_none?
+        self.length.times do |i|
+            if yield self[i]
                 return false
             end
         end
         return true
     end
 
-    def self.my_count array
+    def my_count
         count = 0
-        array.length.times do |i|
-            if yield array[i]
+        self.length.times do |i|
+            if yield self[i]
                 count += 1
             end
         end
         return count
     end
 
-    def self.my_map array
-        array.length.times do |i|
-            array[i] = yield array[i]
+    def my_map
+        self.length.times do |i|
+            self[i] = yield self[i]
         end
-        return array
+        return self
     end
 
-    def self.my_inject array
-        memo = array[0]
-        (array.length-1).times do |i|
-            memo = yield memo, array[i+1]
+    def my_inject
+        memo = self[0]
+        (self.length-1).times do |i|
+            memo = yield memo, self[i+1]
         end
         return memo
     end
 end
 
 def multiply_els array
-    return Enumerable::my_inject (array) { |elem, n| elem * n}
+    return array.my_inject { |elem, n| elem * n}
 end
 
-array = [2,4,5]
+array = [1,2,3,4,5]
 
-p multiply_els (array)
-p array.inject { |elem, n| elem * n}
+# p multiply_els array
+# p array.inject { |elem, n| elem * n}
 
-# p Enumerable::my_inject (array) { |elem, n| elem + n}
+# p array.my_inject { |elem, n| elem + n}
 # p array.inject { |elem, n| elem + n}
 
-# p Enumerable::my_map (array) {|elem| elem + 2}
+# p array.my_map {|elem| elem + 2}
 # p array.map { |elem| elem + 2}
 
-# p Enumerable::my_count (array) {|elem| elem > 2}
+# p array.my_count {|elem| elem > 2}
 # p array.count { |elem| elem > 2}
 
-# p Enumerable::my_none? (array) {|elem| elem == 7}
+# p array.my_none? {|elem| elem == 7}
 # p array.none? {|elem| elem == 7}
 
-# p Enumerable::my_any? (array) { |elem| elem == 4 }
+# p array.my_any? { |elem| elem == 4 }
 # p array.any? { |elem| elem == 4 }
 
-# p Enumerable::my_all? (array) { |elem| elem < 10 }
+# p array.my_all? { |elem| elem < 10 }
 # p array.all? { |elem| elem < 10}
 
-# p Enumerable::my_select (array) { |elem| elem.odd? }
+# p array.my_select { |elem| elem.odd? }
 # p array.select{ |elem| elem.odd? }
 
-# array.each_with_index do |elem,index|
-#     puts "elem: " + elem.to_s + ", index:" + index.to_s
-# end
+# array.each_with_index {|elem,index| p "elem: " + elem.to_s + ", index:" + index.to_s} 
+# array.my_each_with_index { |elem, index| p "elem: " + elem.to_s + ", index:" + index.to_s }
 
-# Enumerable::my_each_with_index (array) do |elem, index| 
-# puts "elem: " + elem.to_s + ", index:" + index.to_s
-# end
+# array.each {|elem| p elem }
+# array.my_each { |elem| p elem } 
 
 
-# array.each do |elem|
-#     puts elem
-# end
-# # 2 4 6 8 10
-# Enumerable::my_each (array) { |elem| puts elem } 
-# # 2 4 6 8 10
+
 
 
